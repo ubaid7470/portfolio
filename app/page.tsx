20ddebbed6c341ed12f2ae0d2586d250541be082
ubaid7470 <obaidkhan2667@gmail.com>
2024-07-24 10:14:24 +0500

    Assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { NavBar } from "@/components/ui/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <NavBar navItems={[]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -22,6 +23,7 @@ const Hero = () => {
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.04] bg-grid-black/[0.2]  flex absolute top-0 left-0 items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
+
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-sx text-center text-blue-100 ">
