e44b018409717598d10d3d4386bb00191b843473
ubaid7470 <obaidkhan2667@gmail.com>
2024-07-31 20:07:49 +0500

    added padding

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -56,7 +56,7 @@ export const NavBar = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-1 md:px-3 py-2 md:py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center text-center sm:space-x-2 md:space-x-4",
+          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-2 md:px-3 py-2 md:py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center text-center sm:space-x-2 md:space-x-4",
           className,
         )}
         style={{
@@ -87,7 +87,7 @@ export const NavBar = ({
                   key={`link=${idx}`}
                   href={navItem.link}
                   className={cn(
-                    "relative dark:text-neutral-50 items-center flex px-1 sm:px-0 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500",
+                    "relative dark:text-neutral-50 items-center flex px-2 sm:px-0 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500",
                   )}
                 >
                   <span className=" text-sm !cursor-pointer">
